b2bb74cbe47ff9acedb549a3d0df806b373a755b
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-05 12:41:48 -0500

    user model

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false
+    }
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
   };
   return User;
