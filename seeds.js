09b914cfbea253ca87ab6a0b2667b47451392c1b
Qingwei <qchen6060@gmail.com>
2021-04-06 17:47:45 -0400

    keep seeding

--- seeds.js
+++ seeds.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 let users = []
 
-async function findUsers() {
+async function createUsers() {
     let test = await fetch("https://swapi.dev/api/people")
     let people = await test.json()
     let persons = people.results.forEach((person) => {
@@ -122,56 +122,75 @@ let activities = [
     "picnicing",
     "Sightseeing"]
 
-let states = [
-"Alabama,AL"
-Alaska, "AK"
-Arizona,	AZ
-Arkansa,	AR
-Califor,"	CA
-Colorado	CO
-Connecticut	CT
-Delaware	DE
-Florida	FL
-Georgia	GA
-Hawaii	HI
-Idaho	ID
-Illinois	IL
-Indiana	IN
-Iowa	IA
-Kansas	KS
-Kentucky	KY
-Louisiana	LA
-Maine	ME
-Maryland	MD
-Massachusetts	MA
-Michigan	MI
-Minnesota	MN
-Mississippi	MS
-Missouri	MO
-Montana	MT
-Nebraska	NE
-Nevada	NV
-New Hampshire	NH
-New Jersey	NJ
-New Mexico	NM
-New York	NY
-North Carolina	NC
-North Dakota	ND
-Ohio	OH
-Oklahoma	OK
-Oregon	OR
-Pennsylvania	PA
-Rhode Island	RI
-South Carolina	SC
-South Dakota	SD
-Tennessee	TN
-Texas	TX
-Utah	UT
-Vermont	VT
-Virginia	VA
-Washington	WA
-West Virginia	WV
-Wisconsin	WI
-Wyoming
-]
-findUsers();
+// let states = [
+// "Alabama,AL"
+// Alaska, "AK"
+// Arizona,	AZ
+// Arkansa,	AR
+// Califor,"	CA
+// Colorado	CO
+// Connecticut	CT
+// Delaware	DE
+// Florida	FL
+// Georgia	GA
+// Hawaii	HI
+// Idaho	ID
+// Illinois	IL
+// Indiana	IN
+// Iowa	IA
+// Kansas	KS
+// Kentucky	KY
+// Louisiana	LA
+// Maine	ME
+// Maryland	MD
+// Massachusetts	MA
+// Michigan	MI
+// Minnesota	MN
+// Mississippi	MS
+// Missouri	MO
+// Montana	MT
+// Nebraska	NE
+// Nevada	NV
+// New Hampshire	NH
+// New Jersey	NJ
+// New Mexico	NM
+// New York	NY
+// North Carolina	NC
+// North Dakota	ND
+// Ohio	OH
+// Oklahoma	OK
+// Oregon	OR
+// Pennsylvania	PA
+// Rhode Island	RI
+// South Carolina	SC
+// South Dakota	SD
+// Tennessee	TN
+// Texas	TX
+// Utah	UT
+// Vermont	VT
+// Virginia	VA
+// Washington	WA
+// West Virginia	WV
+// Wisconsin	WI
+// Wyoming
+// ]
+
+
+
+
+async function createPosts(num){
+    let description = [];
+    while(num){
+        let postPath = await fetch(`https://hipsum.co/api/?type=hipster-centric&sentences${Math.floor(Math.random()*(5-2)+2)}&paras=${Math.floor(Math.random()*(5-2)+2)}`)
+        let posts = await postPath.json()
+        description.push(posts)
+        num--
+    }
+    console.log(description);
+
+
+
+}
+  createPosts(10)
+
+createUsers();
