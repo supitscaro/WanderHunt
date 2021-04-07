4af2645d9b6edaa588a0c99ffd934446a506d099
jdaniel01 <professor4cob@outlook.com>
2021-04-07 13:06:13 -0700

    changed colors

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
-const {Post, User, State, Activity, Comment} = require('.../db/models');
-window.addEventListener("load", (event)=>{
-    async function getPosts(){
-        let posts = await Post.findAll({include: {User, State, Activity}});
-        posts.forEach(post => {
-            console.log(post.title);
-        })
-    }
+// const {Post, User, State, Activity, Comment} = require('.../db/models');
+// window.addEventListener("load", (event)=>{
+//     async function getPosts(){
+//         let posts = await Post.findAll({include: {User, State, Activity}});
+//         posts.forEach(post => {
+//             console.log(post.title);
+//         })
+//     }
 
 
 
-})
+// })
