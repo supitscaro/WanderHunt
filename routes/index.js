4d65c829b2189390b36b94ea26633e48ad40a123
jdaniel01 <professor4cob@outlook.com>
2021-04-07 12:17:14 -0700

    working lunch

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const {Post} = require('../db/models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  const posts = Post.findAll({where: {
+    order
+  }}
+  res.render('index', { title: 'Wanderhunt' });
 });
 
 module.exports = router;
