a91aa08436e7f46ac5b7e030a694e6b25d590196
jdaniel01 <professor4cob@outlook.com>
2021-04-07 15:51:19 -0700

    changing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const {Post} = require('../db/models');
-const {getPosts} = require('../public/javascripts/index')
+const {Post, User, State, Activity} = require('../db/models');
+// const findPosts = require('../public/javascripts/index')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const posts = Post.findAll()
-  res.render('index', { title: 'Wanderhunt' });
+router.get('/', async function(req, res, next) {
+    // findPosts();
+    const posts = Post.findAll({order: ['createdAt', 'DESC']}
+    );
+  res.render('index', { title: 'Wanderhunt', posts}, );
 });
 
 module.exports = router;
