fad8a9bec87f2691d755acc59af83602efd713ee
jdaniel01 <professor4cob@outlook.com>
2021-04-08 08:27:54 -0700

    up to date

--- db/migrations/20210406154750-create-post.js
+++ db/migrations/20210406154750-create-post.js
@@ -17,12 +17,7 @@ module.exports = {
         allowNull: false
       },
       gallery: {
-<<<<<<< HEAD:db/migrations/20210406154753-create-post.js
-        type: Sequelize.TEXT,
-        allowNull: false
-=======
         type: Sequelize.STRING
->>>>>>> main:db/migrations/20210406154750-create-post.js
       },
       user_id: {
         type: Sequelize.INTEGER,
@@ -32,20 +27,12 @@ module.exports = {
       state_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-<<<<<<< HEAD:db/migrations/20210406154753-create-post.js
-        references: { model: "Activities" }
-=======
         references: {model: "States"}
->>>>>>> main:db/migrations/20210406154750-create-post.js
       },
       activity_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-<<<<<<< HEAD:db/migrations/20210406154753-create-post.js
-        references: { model: "States" }
-=======
         references: {model: "Activities"}
->>>>>>> main:db/migrations/20210406154750-create-post.js
       },
       createdAt: {
         allowNull: false,
