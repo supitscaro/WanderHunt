1cf2274472cb3ace7011cac9fe88242138650f39
jdaniel01 <professor4cob@outlook.com>
2021-04-08 15:40:53 -0700

    bringing in nav-bar

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', asyncHandler(async (req, res, next) => {
   let popular = mostPopular(comments);
   let popularPosts = await getPosts(popular);
   let recentPosts = await getRecent();
+  let ativities = await topActivities();
+  let activityPosts = await getActivityPosts(activities);
   res.render('index', { title: 'Wanderhunt', popularPosts: popularPosts, recentPosts: recentPosts });
 }));
 
@@ -37,7 +39,7 @@ function mostPopular(comments) {
     if (challengePostId === comments[i].post_id) {
       tracker[comments[i].post_id]++;
     }
-    if (tracker[challengePostId] > tracker[currentPostId] && !popularPosts.includes(tracker[challengePostId])) {
+    if (tracker[challengePostId] > tracker[currentPostId] && !popularPosts.includes(challengePostId)) {
       currentPostId = challengePostId
       challengePostId = null;
     }
@@ -67,16 +69,61 @@ async function getRecent() {
   const recent = await Post.findAll({ order: ['createdAt'], limit: 10, include: User });
   return recent;
 }
-function topActivitiess(posts) {
 
+async function topActivities() {
+  let popActivities = []
+  let posts = await Post.findAll({ order: ['activity_id']})
+
+
+  let currentActivityId;
+  let challengeActivityId;
+  let tracker = {}
+
+  for (let i = 0; i < posts.length && popActivities.length < 5; i++) {
+    if (!tracker[posts[i].activity_id]) {
+      tracker[posts[i].activity_id] = [1]
+    }
+    if (!currentActivityId) {
+      currentActivityId = posts[i].activity_id;
+    }
+    if (currentActivityId === posts[i].activity_id) {
+      tracker[posts[i].activity_id]++;
+    } else {
+      challengeActivityId = posts[i].activity_id
+      tracker[posts[i].activity_id]++;
+    }
+    if (challengeActivityId === posts[i].activity_id) {
+      tracker[posts[i].activity_id]++;
+    }
+    if (tracker[challengeActivityId] > tracker[currentActivityId] && !popActivities.includes(challengeActivityId)) {
+      currentActivityId = challengeActivityId
+      challengeActivityId = null;
+    }
+    if (i === posts.length - 1) {
+
+      popActivities.push(currentActivityId);
+      tracker[currentActivityId] = 0
+      i = 0
+    }
+  }
+
+  return popActivities;
 }
+
+async function getActivityPosts(activityArray){
+  activityPosts = []
+
+  while (activity.length) {
+    let post = await Post.findOne({ where: { id: `${activityArray[0]}` }, include: [Activity] })
+    activityPosts.push(post);
+    activityArray.shift();
+  }
+  return activityPosts;
+}
+
 function topStates(posts) {
 
 }
 
-function sortPopular(array) {
-
-
-}
 
 module.exports = router;
