969a64d1338b4ca8d6745168dbd3111de46615d3
jdaniel01 <professor4cob@outlook.com>
2021-04-08 08:37:29 -0700

    here we go

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,13 @@
-
-document.addEventListener("load", (event)=>{
-
-    async function getPosts(){
-        let result = await fetch('localhost:8080/')
-        let posts = result.json();
-        if (posts){
-            console.log(posts[1].title)
-        }
-    }
-    getPosts();
+window.addEventListener("load", (event)=>{
+    
+    // async function getPosts(){
+    //     let result = await fetch('localhost:8080/')
+    //     let posts = result.json();
+    //     if (posts){
+    //         console.log(posts[1].title)
+    //     }
+    // }
+    // getPosts();
 })
 
-module.exports = getPosts;
+// module.exports = getPosts;
