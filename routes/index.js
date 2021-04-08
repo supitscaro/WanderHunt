3069c89a8f380112321191e8bc999117ed30c3fb
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-07 20:58:32 -0500

    working index.js changes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const {Post} = require('../db/models');
-const {getPosts} = require('../public/javascripts/index')
+const { Post, User } = require('../db/models');
+const { asyncHandler } = require('./utils');
+const { getPosts } = require('../public/javascripts/index')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const posts = Post.findAll()
-  res.render('index', { title: 'Wanderhunt' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  // findPosts();
+  const posts = await Post.findAll({
+    order: [['id', 'DESC']],
+    limit: 10,
+    include: { model: User },
+  }
+  );
+  console.log(posts);
+  res.render('index', { title: 'Wanderhunt', posts },);
+}));
 
 module.exports = router;
