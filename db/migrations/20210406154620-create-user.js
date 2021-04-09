3804352c6b20ce2185804b43a43ffe44d36aaeb4
Zane Preudhomme <zane@effarig.com>
2021-04-08 22:38:36 -0700

    Got the default user photo working

--- db/migrations/20210406154620-create-user.js
+++ db/migrations/20210406154620-create-user.js
@@ -23,7 +23,8 @@ module.exports = {
         allowNull: false
       },
       profilePhoto: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
       },
       createdAt: {
         allowNull: false,
